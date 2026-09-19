5f4a11598bd40d8c06861e779f24339f90d662b7
unknown <tim_critt@hotmail.com>
2026-09-19 02:35:55 +0200

    Made Migration V__10 idempotent

--- src/main/resources/db/migration/V10__remove_FK_contraint_from_table_user_roles.sql
+++ src/main/resources/db/migration/V10__remove_FK_contraint_from_table_user_roles.sql
@@ -1,5 +1,8 @@
 ALTER TABLE user_roles
-    DROP CONSTRAINT fkhfh9dx7w3ubf1co1vdev94g3f;
+    DROP CONSTRAINT IF EXISTS fkhfh9dx7w3ubf1co1vdev94g3f;
+
+ALTER TABLE user_roles
+    DROP CONSTRAINT IF EXISTS fk_user_roles_user;
 
 ALTER TABLE user_roles
     ADD CONSTRAINT fk_user_roles_user
